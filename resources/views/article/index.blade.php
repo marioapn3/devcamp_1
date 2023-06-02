5ac7973f7d6b4b7fc6026fcc2a2d11f653631827
marioapn3 <mario.aprilnino27@gmail.com>
2023-06-02 15:23:04 +0700

    first commit

---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,47 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __("Article") }}
+            </h2>
+            <button type="button" class="btn btn-outline-primary"><a href="{{ route('article.create') }}" >ADD</a></button>
+        </div>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <table class="border-collapse table-auto w-full text-sm">
+                        <thead>
+                            <tr>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">title</th>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Email</th>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Created At</th>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white">
+                            {{-- populate our post data --}}
+                            @foreach ($articles as $article)
+                                <tr>
+                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $article->title }}</td>
+                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $article->description }}</td>
+                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $article->created_at }}</td>
+                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                        <div class="d-flex">
+                                            <button type="button" class="btn btn-outline-warning"><a href="{{ route('article.edit', $article->id) }}">EDIT</a></button>
+                                            <form method="post" action="{{ route('article.destroy', $article->id) }}" class="inline">
+                                                @csrf
+                                                @method('delete')
+                                                <button class="btn btn-outline-danger ml-2 ">DELETE</button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+</x-app-layout>
